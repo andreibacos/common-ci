5e8397e2bf9cc419125940da7df66690aa9d0b30
andrei bacos <andreibacos@gmail.com>
2016-04-01 10:06:53 +0000

    copy logs to log server

--- infra/logs/collect-logs.sh
+++ infra/logs/collect-logs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 BASEDIR=$(dirname $0)
 DEVSTACK_LOGS="/opt/stack/logs/screen"
@@ -14,8 +15,12 @@ LOG_DST_HV="$LOG_DST/Hyper-V-logs"
 CONFIG_DST_DEVSTACK="$LOG_DST/devstack-config"
 CONFIG_DST_HV="$LOG_DST/Hyper-V-config"
 
+LOG_SERVER="root@logs.maas"
+LOG_KEY="/home/ubuntu/log.key"
+
 TAR="tar"
 GZIP="gzip -f"
+SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
 source $BASEDIR/utils.sh
 
@@ -55,3 +60,15 @@ emit_info "Collecting devstack configs"
 archive_devstack_configs
 emit_info "Collecting tempest files"
 archive_tempest_files
+# TODO Hyperv logs and configs
+
+# Archive everything and copy to logs server
+pushd $LOG_DST; tar -zcf "$LOG_DST.tar.gz" .; popd
+
+LOG_SERVER_DIR="/logs/$PROJECT/$CHANGE/$PATCHSET"
+
+ssh $SSH_OPTS -i $LOG_KEY $LOG_SERVER "if [ ! -d $LOG_SERVER_DIR ]; then mkdir -p $LOG_SERVER_DIR; else rm -rf $LOG_SERVER_DIR/*; fi"
+scp $SSH_OPTS -i $LOG_KEY "$LOG_DST.tar.gz" "$LOG_SERVER:$LOG_SERVER_DIR/aggregate.tar.gz"
+ssh $SSH_OPTS -i $LOG_KEY $LOG_SERVER "tar -zxf $LOG_SERVER_DIR/aggregate.tar.gz -C $LOG_SERVER_DIR/"
+
+
